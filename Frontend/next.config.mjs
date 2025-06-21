3a27c7bce37adc7dca68f125dad5e36651b5c9a6
Zaid Ashraf <zaidashraf249@gmail.com>
2025-06-21 19:42:32 +0530

    something changed

--- Frontend/next.config.mjs
+++ Frontend/next.config.mjs
@@ -2,7 +2,7 @@ const isGithubPages = process.env.NODE_ENV === 'production';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  // output: 'export',
   basePath: isGithubPages ? '/updated-portfolio' : '',
   assetPrefix: isGithubPages ? '/updated-portfolio/' : '',
 };
